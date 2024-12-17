72e2c0dd4d54c3dbb4f67e8ccf143b18af50c7de
Toygar Par <toygar.par@gmail.com>
2024-12-17 11:17:59 +0300

    Enum türü intro added

---
+++ enum.c
@@ -0,0 +1,38 @@
+//C Dili Kullanıcı Tanımlı Değişken Türleri - Enum
+
+/*Enum -  Belirli bir sıralı listeden seçenek yapmamıza izin veri yapı türü
+
+Eğitim Düzeyi - İlkokul, Ortaokul, Lise, Ön Lisans, Lisans,Yüksek Lisans, Doktora
+
+Makul değerlerle doldurulması, izin verilmeyen seçenekleri dışarda bırakmak için*/
+
+//programlama yaparken bazen sadece tanımlı değişkenler kullanamanız gerekir
+
+#include <stdio.h>
+
+enum Levels {
+    LO = 14,
+    MID = 19,
+    HI = 24
+};
+
+int main() {
+    //enum türünde bir değişken tanımlama ve değer atama
+
+    enum Levels RoomTemp = HI;
+
+    //enum değişkenini yazdır
+
+    printf("%d\n", RoomTemp);
+    printf("\n...........................\n");
+
+    
+
+
+
+
+
+    return 0;
+}
+
+
